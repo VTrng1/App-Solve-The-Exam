9d224bf2178890f4a7daea40a7710ac5d25d0f8d
Administrator <congthang13022000@gmail.com>
2021-06-30 21:49:09 +0700

    Xảy ra Bug làm lại

--- app/src/main/java/com/example/app_sovle_the_exam/MainActivity.kt
+++ app/src/main/java/com/example/app_sovle_the_exam/MainActivity.kt
@@ -9,9 +9,6 @@ import android.view.WindowManager
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-        WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
     }
 }
