049dbb6c655dc6d3864993c74b841518f0b07889
Administrator <congthang13022000@gmail.com>
2021-06-30 23:57:04 +0700

    Push activity_login.xml and activity_register.xml

---
+++ app/src/main/java/com/example/app_sovle_the_exam/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.app_sovle_the_exam
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
