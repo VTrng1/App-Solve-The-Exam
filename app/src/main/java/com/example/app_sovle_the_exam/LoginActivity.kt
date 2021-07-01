1c26b047995a35e6b58711c53bc75bfc2ad5e7e7
Administrator <congthang13022000@gmail.com>
2021-07-01 21:35:14 +0700

    Push lại Login and Register

--- app/src/main/java/com/example/app_sovle_the_exam/LoginActivity.kt
+++ app/src/main/java/com/example/app_sovle_the_exam/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.app_sovle_the_exam
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/app_sovle_the_exam/RegisterActivity.kt
+++ app/src/main/java/com/example/app_sovle_the_exam/RegisterActivity.kt
@@ -3,9 +3,10 @@ package com.example.app_sovle_the_exam
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
     }
 }
